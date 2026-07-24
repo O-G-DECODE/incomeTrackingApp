d52b2fa7bb3574d03bc0785bc776a67cb64a612b
oganand <anandog12@gmail.com>
2026-07-24 22:12:26 +0530

    project started

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -24,11 +24,18 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="admin"
         options={{
-          title: 'Explore',
+          title: 'Admin',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
+      />
+      <Tabs.Screen
+      name="bills"
+      options={{
+        title:"Bills",
+        tabBarIcon: ({color}) => <IconSymbol size={28} name="doc.text.fill" color={color} />
+      }}
       />
     </Tabs>
   );
