3c6f80c0dd0dd4fc751c153471a390684f33b489
oganand <anandog12@gmail.com>
2026-07-28 23:06:46 +0530

    bug: code added to run /test app in emulator

--- database/db.ts
+++ database/db.ts
@@ -1,5 +1,46 @@
 import * as SQLite from "expo-sqlite";
 
+let database: SQLite.SQLiteDatabase | null = null;
+
 export async function getDatabase() {
-  return await SQLite.openDatabaseAsync("billing_v2.db");
+
+  try {
+
+    if (database) {
+      return database;
+    }
+
+    console.log("Opening SQLite database...");
+
+    database = await SQLite.openDatabaseAsync(
+      "billing_v2.db"
+    );
+
+
+    await database.execAsync(`
+      CREATE TABLE IF NOT EXISTS Products (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        productName TEXT NOT NULL,
+        fullPrice REAL NOT NULL,
+        halfPrice REAL,
+        quarterPrice REAL,
+        createdAt TEXT NOT NULL
+      );
+    `);
+
+
+    console.log("✅ Database ready");
+
+    return database;
+
+
+  } catch(error) {
+
+    console.error(
+      "❌ Database initialization error:",
+      error
+    );
+
+    throw error;
+  }
 }
